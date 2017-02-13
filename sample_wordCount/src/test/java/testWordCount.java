ae8a2be2ee9b06d53082d032343489c13175d627
isaac <isaac.chan13@gmail.com>
2017-02-12 16:07:16 -0800

    sample word count example

---
+++ sample_wordCount/src/test/java/testWordCount.java
@@ -0,0 +1,13 @@
+/**
+ * Created by isaac on 2/8/17.
+ */
+
+import junit.framework.*;
+
+public class testWordCount extends TestCase {
+
+    public void test(){
+
+        //assertTrue(result == 6);
+    }
+}
